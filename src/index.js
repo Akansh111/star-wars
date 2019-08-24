b81ccbcc9a0f315899c808d120fd8aa0d658c266
Akansh Goel <akansh.goel@magicsw.com>
2019-08-25 01:58:48 +0530

    removed redux

--- src/index.js
+++ src/index.js
@@ -6,22 +6,16 @@ import Search from './containers/Search/Search';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
-import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
 
 const routing = (
-    <Provider store={store}>
         <Router>
             <div>
                 <Route path="/login" component={Login} />
                 <Route path="/search" component={Search} />
             </div>
         </Router>
-    </Provider>
 )
 
 ReactDOM.render(routing, document.getElementById('root'));
